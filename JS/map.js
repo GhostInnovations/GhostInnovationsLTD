3493f8bcaa482bd34d25fbdf93f8d7f152be0d41
Alex <M2504834@middlesbro.ac.uk>
2026-07-03 13:10:54 +0100

    update

--- JS/map.js
+++ JS/map.js
@@ -1,14 +1,10 @@
-// Initialize the map to be at the office building
-const map = L.map('map').setView([54.57932, -1.23138], 16);
+const map = L.map('map').setView([54.5820213, -1.1771227], 16);
 
-// Use OpenStreetMap's official tile server - requires no API key
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Add a marker at office building
-const marker = L.marker([54.57932, -1.23138]).addTo(map);
+const marker = L.marker([54.5820213, -1.1771227]).addTo(map);
 
-// Add a popup with the location name
-marker.bindPopup("<b>Boho 5</b><br>Bridge Street East<br>Middlesbrough, TS2 1NY").openPopup();
+marker.bindPopup("<b>Units 3, & 4</b><br>North St<br>Middlesbrough TS6 6AN").openPopup();
